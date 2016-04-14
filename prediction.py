f897b87b91d0295de0c791675e89913898fc56fc
Camille WAGNER <cwagner@e3r9p17.42.fr>
2016-04-14 17:19:47 +0200

    FileNotFound errors corrected

--- prediction.py
+++ prediction.py
@@ -1,10 +1,20 @@
+import sys
+
 def main():
     theta_file = "thetas.txt"
-    with open(theta_file) as f:
-        t0, t1, mi, ma = [float(i.strip()) for i in f.readlines()]
-        print ("I follow the linear function: '{} * x + {}'".format(t0, t1))
-        mil = int(input("Mileage : ").strip())
-        print ("Estimated price is: ", t0 + t1*(((mil - mi) / (ma - mi))))
+    try:
+        with open(theta_file) as f:
+            try:
+                t0, t1, mi, ma = [float(i.strip()) for i in f.readlines()]
+            except ValueError:
+                sys.exit("The file was ill-formed, please relaunch training")
+            print ("I follow the linear function: '{} * x + {}'".format(t0, t1))
+            mil = int(input("Mileage : ").strip())
+            print ("Estimated price is: ", t0 + t1*(((mil - mi) / (ma - mi))))
+    except FileNotFoundError:
+        print ("No training was done before, the result will be 0")
+        input("Mileage : ")
+        print ("Estimated price is: 0")
 
 if __name__ == '__main__':
     main()
